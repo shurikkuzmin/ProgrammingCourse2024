33cb0927e30210498b6e3363f83ee59b71420020
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2024-10-18 22:56:16 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# ProgrammingCourse2024
---
+++ hello.py
@@ -0,0 +1,8 @@
+name = input("Please enter your name: ")
+#print("Hello, " + name + "!")
+#print(name)
+
+if name == "Alex":
+    print("Hello, Alex!")
+else:
+    print("Get the hell out of here!")
