ba5f371b8d5970da77c1c3563689e18f6ce7102f
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2024-11-09 10:33:36 -0500

    Hangman textual is ready.

--- hangman.py
+++ hangman.py
@@ -1,12 +1,24 @@
-import random
+import tkinter
 
-array = ["apple","orange","donkey","monkey","capibara","leopard"]
-computer_word = random.choice(array)
-my_word = len(computer_word)*'*'
+window = tkinter.Tk()  
+window.geometry('600x600')  
+window.title('Hangman')
+window.mainloop()
 
-# for attempt in range(5):
+# import random
+
+# array = ["apple","orange","donkey","monkey","capibara","leopard"]
+# computer_word = random.choice(array)
+# my_word = len(computer_word)*'*'
+
+# for attempt in range(int(1.5 * len(computer_word))):
 #     print(my_word)
 #     letter = input("Please enter a letter: ")
 #     for i in range(len(computer_word)):
 #         if letter == computer_word[i]:
-#             my_word[i] = letter[0]
+#             my_word = my_word[:i]+letter+my_word[i+1:]
+#     if my_word == computer_word:
+#         print("You've got it!")
+#         break
+# else:
+#     print("You are loser!")
