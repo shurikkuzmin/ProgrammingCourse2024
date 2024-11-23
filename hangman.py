2523a4bded7dbad55e2beb275924b2dd7a00d5d2
Alexandr Kuzmin <shurik.kuzmin@gmail.com>
2024-11-23 10:33:37 -0500

    Adding text on the screen.

--- hangman.py
+++ hangman.py
@@ -2,8 +2,14 @@ import pygame
 import pygame.display
 import pygame.image
 import pygame.transform
+import pygame.font
+import random
 
 pygame.init()
+pygame.font.init() 
+
+font = pygame.font.SysFont('Comic Sans MS', 150)
+#font_won = pygame.font.SysFont('Ariel',300)
 
 background = pygame.image.load("h0.png")
 background = pygame.transform.scale(background,(1000, 1000))
@@ -12,14 +18,28 @@ screen_width = background.get_width()
 screen_height = background.get_height()
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+array = ["apple","orange","donkey","monkey","capibara","leopard"]
+computer_word = random.choice(array)
+my_word = len(computer_word)*'*'
 while True:
     screen.fill((102,2,60))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
+        if event.type == pygame.KEYDOWN:
+            letter = pygame.key.name(event.key)
+            for i in range(len(computer_word)):
+                if letter == computer_word[i]:
+                    my_word = my_word[:i]+letter+my_word[i+1:]
+    text = font.render(my_word,True,(255,69,0))
     screen.blit(background,(0,0))
+    screen.blit(text,(550,120))
+    if my_word == computer_word:
+        break
+
     pygame.display.update()
+
 # import tkinter
 # from PIL import Image, ImageTk
 
